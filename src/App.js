13eb289427e24d1d665465a08b6417ba49edc1f4
Xearta <rsmcwow@gmail.com>
2020-08-11 20:06:20 -0400

    Created product component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className='grid-container'>
-      <header>
-        <a href='/'>React Shopping App</a>
-      </header>
-      <main>Product List</main>
-      <footer>All rights are reserved.</footer>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    products: data.products,
+    size: '',
+    sort: '',
+  };
+
+  render() {
+    return (
+      <div className='grid-container'>
+        <header>
+          <a href='/'>React Shopping App</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>
+              <Products products={this.state.products} />
+            </div>
+            <div className='sidebar'>Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights are reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
